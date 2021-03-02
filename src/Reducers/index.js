9c815e165893a04e2a8f5b707f8ad83e00da55da
Nirnay Jain <nirujainofficial@gmail.com>
2021-03-02 19:37:08 +0530

    Updates

--- src/Reducers/index.js
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from 'redux'
-import {gameReducer} from "./gameReducer"
-import {detailReducer} from './detailReducer'
-const rootReducer= combineReducers({
-    games : gameReducer,
-    details: detailReducer,
-});
-export default rootReducer; 
