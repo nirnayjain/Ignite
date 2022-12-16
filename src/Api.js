57fa05d7845a411e549d2a3964b852179c8b794e
Nirnay Jain <nirujainofficial@gmail.com>
2022-12-16 14:36:42 +0530

    api updated

--- src/Api.js
+++ src/Api.js
@@ -1,4 +1,4 @@
-const  base_url="https://api.rawg.io/api";
+const  base_url="https://api.rawg.io/api/";
 const getCurrentMonth=()=>{
     const month=new Date().getMonth()+1;
     if(month<10)
@@ -27,14 +27,14 @@ const previousYear=`${currentYear-1}-${currentMonth}-${currentDay}`
 const nextYear=`${currentYear+1}-${currentMonth}-${currentDay}`
  
 
-const popularGames=`games?dates=${previousYear},${currentDate}&ordering=-rating&page_size=10`;
-const upcomingGames=`games?dates=${currentDate},${nextYear}&ordering=-added&page_size=10`;
-const newGames=`games?dates=${previousYear},${currentDate}&ordering=-released&page_size=10`;
+const popularGames=`games?dates=${previousYear},${currentDate}&ordering=-rating&page_size=10&key=8fb5a06509c24378ad37cfbc27225495`;
+const upcomingGames=`games?dates=${currentDate},${nextYear}&ordering=-added&page_size=10&key=8fb5a06509c24378ad37cfbc27225495`;
+const newGames=`games?dates=${previousYear},${currentDate}&ordering=-released&page_size=10&key=8fb5a06509c24378ad37cfbc27225495`;
 
 
  export const popularGamesUrl=()=>`${base_url}${popularGames}`;
  export const upcomingGamesUrl=()=>`${base_url}${upcomingGames}`;
  export const newGamesUrl=()=>`${base_url}${newGames}`;
- export const gameDetailsUrl=(game_id)=>`${base_url}games/${game_id}`;
- export const screenshotUrl=(game_id)=>`${base_url}games/${game_id}/screenshots`;
- export const searchGameUrl=(game_name)=>`${base_url}games?search=${game_name}&page_size=9`;
+ export const gameDetailsUrl=(game_id)=>`${base_url}games/${game_id}?key=8fb5a06509c24378ad37cfbc27225495`;
+ export const screenshotUrl=(game_id)=>`${base_url}games/${game_id}/screenshots?key=8fb5a06509c24378ad37cfbc27225495`;
+ export const searchGameUrl=(game_name)=>`${base_url}games?search=${game_name}&page_size=9&key=8fb5a06509c24378ad37cfbc27225495`;
